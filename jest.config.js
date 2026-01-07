106122d2d1d50ca267466114c2716ffb3959cd5d
priyalr <priya@logicrays.com>
2026-01-07 14:53:05 +0530

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   preset: 'react-native',
+
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native' +
+      '|@react-native' +
+      '|@revopush/react-native-code-push' +
+      ')/)',
+  ],
+
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 };
